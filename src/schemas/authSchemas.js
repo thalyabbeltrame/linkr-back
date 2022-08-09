038c7b362e52d002a363e18d0d9b40bed4c884a7
Yorí <yorigcsdev@gmail.com>
2022-08-09 03:54:40 -0300

    feat: create signUpSchema

---
+++ src/schemas/authSchemas.js
@@ -0,0 +1,9 @@
+import Joi from 'joi'
+
+export const signUpSchema = Joi.object({
+  username: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+  image: Joi.string().uri().required()
+})
+
