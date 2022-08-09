7b205225c99db65ff56b5e9e71b1408c87cf465c
Yorí <yorigcsdev@gmail.com>
2022-08-09 05:07:39 -0300

    feat: add route signin to authRouter

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { validateSchema } from '../middlewares/schemaValidate.js'
-import { signUpSchema } from '../schemas/authSchemas.js'
-import { createUser } from '../controllers/authController.js'
+import { signUpSchema, signInSchema } from '../schemas/authSchemas.js'
+import { allowLogin, createUser } from '../controllers/authController.js'
 const authRouter = Router()
 
 authRouter.post('/signup', validateSchema(signUpSchema), createUser)
+authRouter.post('/signin', validateSchema(signInSchema), allowLogin)
 
 export default authRouter
