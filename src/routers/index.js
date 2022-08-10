836b41b80f39e88e44aa114b6953cb694e1a77df
Marcus Vinicius de Souza Soares <60653332+marcus-souza-soares@users.noreply.github.com>
2022-08-10 12:24:01 -0300

    Revert "Send users search input"

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,11 +2,9 @@ import { Router } from 'express';
 
 import authRouter from './authRouter.js';
 import timelineRouter from './timelineRouter.js';
-import userRouter from "./usersRouter.js"
 
 const router = Router();
 router.use('/', authRouter);
 router.use('/', timelineRouter);
-router.use('/', userRouter);
 
 export default router;
--- src/routers/usersRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import validateToken from '../middlewares/tokenMiddleware.js';
-import {getUsersListByName} from "../controllers/usersContoller.js"
-
-const userRouter = Router();
-
-userRouter.get("/users/:name", validateToken ,getUsersListByName);
-
-export default userRouter;
