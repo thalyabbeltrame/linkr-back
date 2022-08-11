794eae11544d84203a63bea308e8338d160f8fb6
Thalya Busato Beltrame <beltrame.thalya@gmail.com>
2022-08-11 17:25:45 -0300

    feat: add the number of likes to posts

--- src/schemas/postSchemas.js
+++ src/schemas/postSchemas.js
@@ -1,8 +1,10 @@
 import Joi from 'joi';
 
 export const postSchema = Joi.object({
-    link: Joi.string()
-        .pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,256}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/)
-        .required(),
-    text: Joi.string(),
+  link: Joi.string()
+    .pattern(
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,256}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
+    )
+    .required(),
+  text: Joi.string(),
 });
