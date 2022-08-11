c8efda1bbb33b675cd930b6efba29ac217eac269
Yorí <yorigcsdev@gmail.com>
2022-08-11 18:02:49 -0300

    fix: ensure that returns correct error

--- src/middlewares/tokenMiddleware.js
+++ src/middlewares/tokenMiddleware.js
@@ -1,32 +1,25 @@
-import jwt from 'jsonwebtoken';
-
-import '../config/index.js';
+import { decodeToken } from '../utils/jwtToken.js'
 import * as usersRepository from '../repositories/usersRepository.js';
 
 const validateToken = async (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  const token = req.header("Authorization")?.split('Bearer ')[1];
+  const decoded = decodeToken(token);
+  if (!decoded) {
     return res.status(401).json({
       error: 'Unauthorized',
     });
   }
-
   try {
-    const token = authorization.replace('Bearer ', '');
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const { rows: users } = await usersRepository.getUserById(decoded.id);
-
     if (!users[0]) {
       return res.status(401).json({
         error: 'Unauthorized',
       });
     }
-
     res.locals.userId = users[0].id;
-
     next();
   } catch (error) {
-    res.status(501).json({
+    res.status(500).json({
       error: error.message,
     });
   }
