dba3ceaea07e96e2518748f99e41c563d89b6a71
Yorí <yorigcsdev@gmail.com>
2022-08-12 01:03:12 -0300

    feat: add hashTagsRouter to index

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,10 +3,11 @@ import { Router } from 'express';
 import authRouter from './authRouter.js';
 import timelineRouter from './timelineRouter.js';
 import userRouter from "./usersRouter.js"
-
+import hashTagsRouter from './hashTags.js';
 const router = Router();
 router.use('/', authRouter);
 router.use('/', timelineRouter);
 router.use('/', userRouter);
+router.use('/',hashTagsRouter)
 
 export default router;
