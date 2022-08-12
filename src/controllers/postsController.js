ad1edd0b2a0ea99599f657a26787a1ad0ab3d46e
Yorí <yorigcsdev@gmail.com>
2022-08-12 20:27:45 -0300

    feat: create updatePostDescription controller

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,4 +1,6 @@
 import * as postsRepository from '../repositories/postsRepository.js';
+import * as hashtagsRepository from '../repositories/hashtagsRepository.js';
+import { handleHashtags } from '../utils/handleHashtags.js';
 
 export const likeDislikePost = async (req, res) => {
   const { id } = req.params;
@@ -13,6 +15,30 @@ export const likeDislikePost = async (req, res) => {
       await postsRepository.createLike(id, userId);
     }
 
+    res.status(200).send('Ok');
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+export const updatePostDescription = async (req, res) => {
+  const { postId } = req.params;
+  const { userId } = res.locals;
+  const { text } = req.body;
+
+  if (!text) {
+    return res.status(422).send('field text in body must be provided');
+  }
+  try {
+    const { rows: user } = await postsRepository.getPostUser(postId);
+    if (user[0]?.user_id !== userId) {
+      return res.status(401).send('You can only edit your own posts!');
+    }
+    await postsRepository.updatePostDescription(postId, text);
+
+    const { rows: hashtags } = await hashtagsRepository.getHashtags();
+    await handleHashtags(hashtags, text, postId);
     res.status(200).send('Ok');
   } catch (error) {
     res.status(500).json({ error: error.message });
