b02b1926a49dcd56922a6add53cfd56d29220582
Yorí <yorigcsdev@gmail.com>
2022-08-12 01:02:47 -0300

    feat: create  router hashTagsRouter

---
+++ src/routers/hashTags.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import { getTagsTrending } from '../controllers/hashTagsController.js';
+import validateToken from '../middlewares/tokenMiddleware.js';
+
+const hashTagsRouter = Router();
+
+hashTagsRouter.get('/trending', validateToken, getTagsTrending);
+
+export default hashTagsRouter;
