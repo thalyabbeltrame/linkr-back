319511f11c2a47f173fcfba791de279c8b8e59c6
Yorí <yorigcsdev@gmail.com>
2022-08-16 18:09:25 -0300

    feat: create followController -> handle follow/unfollow

---
+++ src/controllers/followController.js
@@ -0,0 +1,56 @@
+import * as followRepository from '../repositories/followRepository.js';
+
+export const getFollower = async (req, res) => {
+    const { userId } = res.locals;
+    const { followerId } = req.params;
+    let isFollowing = true;
+    try {
+        if (userId === followerId) {
+            return res.status(200).json({ isFollowing: false })
+        }
+        const { rows: follow } = await followRepository.getFollow(followerId, userId);
+        if (!follow[0]) {
+            return res.status(200).json({ isFollowing: false })
+        }
+        res.status(200).json({ isFollowing: true })
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+export const follow = async (req, res) => {
+    const { userId } = res.locals;
+    const { followerId } = req.params;
+    try {
+        if (userId === followerId) {
+            return res.status(400).send("You can't follow yourself")
+        }
+        const { rows: follow } = await followRepository.getFollow(followerId, userId);
+        if (follow[0]) {
+            return res.status(400).send("You're already following this user!")
+        }
+        await followRepository.followUser(followerId, userId);
+        res.sendStatus(200)
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+export const unfollow = async (req, res) => {
+    const { userId } = res.locals;
+    const { followerId } = req.params;
+    try {
+        if (userId === followerId) {
+            return res.status(400).send("You can't unfollow yourself")
+        }
+        const { rows: follow } = await followRepository.getFollow(followerId, userId);
+        if (!follow[0]) {
+            return res.status(400).send("You're not following this user!")
+        }
+        await followRepository.unfollowUser(followerId, userId);
+        res.sendStatus(200)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: error.message });
+    }
+};
